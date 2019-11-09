c8b20e7eb499c60a9e58206f05fb48c6081b062e
Andre-Home <adotmorrall@gmail.com>
2019-11-09 10:00:39 -0600

    Initial main.js file

---
+++ assets/css/javascript/main.js
@@ -0,0 +1,20 @@
+// Refer to 06-Ajax activities + Panoptop video
+
+var cars = ['audio', 'porsche', 'challenger'];
+
+function showCarButtons() {
+    for (var i = 0; i < cars.length; i++) {
+        console.log(cars[i]);
+    }
+}
+showCarButtons();
+
+// var queryURL = 'https://api.giphy.com/v1/gifs/trending?api_key=ccxCr3q18LuZy8wDnC0dYPLWYBENh5Rz'
+
+// $.ajax({
+//     url: queryURL,
+//     method: 'GET'
+// }).then(function (response) {
+//     console.log(response);
+// })
+
