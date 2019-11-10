8cb0f9acd3776beba4394cd56b67302141ece98d
Andre-Home <adotmorrall@gmail.com>
2019-11-10 11:56:47 -0600

    More updates to main.js

--- assets/css/javascript/main.js
+++ assets/css/javascript/main.js
@@ -1,20 +1,41 @@
 // Refer to 06-Ajax activities + Panoptop video
 
-var cars = ['audio', 'porsche', 'challenger'];
+var cars = ['audi', 'porsche', 'challenger'];
 
 function showCarButtons() {
     for (var i = 0; i < cars.length; i++) {
         console.log(cars[i]);
+        // Create buttons to show text of cars in the #gif-buttons div
+        var c = $('<button>');
+        c.addClass('btn-click');
+        c.text(cars[i]);
+        $('#gif-buttons').append(c);
     }
 }
 showCarButtons();
 
-// var queryURL = 'https://api.giphy.com/v1/gifs/trending?api_key=ccxCr3q18LuZy8wDnC0dYPLWYBENh5Rz'
+$('#submit-btn').click(function (event) {
+    event.preventDefault(); // Keeps page from refreshing
+    var userInput = $('#gif-search').val().trim();
+    if (userInput) {
+        cars.push(userInput);
+        $('#gif-search').val(''); // Getter
+        $('#gif-buttons').empty(); // Keeps from showing duplicate btns
+        showCarButtons();
+    }
+});
 
-// $.ajax({
-//     url: queryURL,
-//     method: 'GET'
-// }).then(function (response) {
-//     console.log(response);
-// })
+$(document).on('click', '.btn-click', function () {
+    console.log('I work')
+    var userCarInput = $(this).text();
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=ccxCr3q18LuZy8wDnC0dYPLWYBENh5Rz&limit=9&q=' + userCarInput;
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function (response) {
+        console.log(response);
+    })
+})
+
 
