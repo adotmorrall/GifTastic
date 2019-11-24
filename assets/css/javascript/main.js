8581626cec979c7c152e5d94c747f2eea2ac0865
Andre <andre.morrall@icloud.com>
2019-11-24 14:07:21 -0600

    Update main.js

--- assets/css/javascript/main.js
+++ assets/css/javascript/main.js
@@ -1,41 +1,50 @@
 // Refer to 06-Ajax activities + Panoptop video
 
-var cars = ['audi', 'porsche', 'challenger'];
+var athletes = ['lebron james', 'kobe bryant', 'michael jordan', 'odell beckham jr', 'simone biles', 'floyd mayweather', 'dwayne wade', 'ken griffey jr', 'tom brady', 'larry bird', 'penny hardaway', 'nolan ryan', 'evander holyfield'];
 
-function showCarButtons() {
-    for (var i = 0; i < cars.length; i++) {
-        console.log(cars[i]);
-        // Create buttons to show text of cars in the #gif-buttons div
-        var c = $('<button>');
-        c.addClass('btn-click');
-        c.text(cars[i]);
-        $('#gif-buttons').append(c);
+function showAthleteButton() {
+    for (var i = 0; i < athletes.length; i++) {
+        // console.log(athletes[i]);
+        // Create buttons to show text of athletes in the #gif-buttons div
+        var a = $('<button>');
+        a.addClass('btn-click');
+        a.text(athletes[i]);
+        $('#gif-buttons').append(a);
     }
 }
-showCarButtons();
+showAthleteButton();
 
 $('#submit-btn').click(function (event) {
     event.preventDefault(); // Keeps page from refreshing
     var userInput = $('#gif-search').val().trim();
     if (userInput) {
-        cars.push(userInput);
+        athletes.push(userInput);
         $('#gif-search').val(''); // Getter
         $('#gif-buttons').empty(); // Keeps from showing duplicate btns
-        showCarButtons();
+        showAthleteButton();
     }
 });
 
 $(document).on('click', '.btn-click', function () {
     console.log('I work')
-    var userCarInput = $(this).text();
-    var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=ccxCr3q18LuZy8wDnC0dYPLWYBENh5Rz&limit=9&q=' + userCarInput;
+    var userAthleteInput = $(this).text();
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=ccxCr3q18LuZy8wDnC0dYPLWYBENh5Rz&limit=9&q=' + userAthleteInput;
+
 
     $.ajax({
         url: queryURL,
         method: 'GET'
     }).then(function (response) {
-        console.log(response);
+        var fixedHeight = response.data[0].images.fixed_height.url;
+        console.log(fixedHeight); //https://developers.giphy.com/docs/api/schema/#image-object
+
+        // Test to load gif into the 
+        var addGif = $('<img>');
+        var gifDiv = addGif.attr('src', fixedHeight);
+        $('#gif-section').append(gifDiv)
+
     })
+
 })
 
 
